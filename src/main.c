d2989cd30405bd78031626c19d5dc1e6ed9e475d
Matías Israelson <57065102+israpps@users.noreply.github.com>
2026-01-10 22:47:34 -0300

    move mmce stuff to it's own header

--- src/main.c
+++ src/main.c
@@ -376,29 +376,7 @@ static void Show_About_uLE(void)
 }
 
 #ifdef MMCE
-#define VMC_NORMAL    0
-#define VMC_BOOTCARD  1
-
-#define SET_SPECIFIC  0
-#define SET_NEXT_CARD 1
-#define SET_PREV_CARD 2
-#define BUILD_SETCARD(param, type, mode, num) param  = type << 24; param |= mode << 16; param |= num;
-#define BUILD_SETCHANNEL(param, mode, num) param  = mode << 16; param |= num;
-enum mmceman_cmds {
-    MMCE_CMD_PING = 0x1,
-    MMCE_CMD_GET_STATUS,
-    MMCE_CMD_GET_CARD,
-    MMCE_CMD_SET_CARD,
-    MMCE_CMD_GET_CHANNEL,
-    MMCE_CMD_SET_CHANNEL,
-    MMCE_CMD_GET_GAMEID,
-    MMCE_CMD_SET_GAMEID,
-    MMCE_CMD_RESET,
-    MMCE_SETTINGS_ACK_WAIT_CYCLES,
-    MMCE_SETTINGS_SET_ALARMS,
-};
-
-#define MAX_GAMEID_LEN 256
+#include "mmce_cmd.h"
 struct cardinfo_t {
 	char* product;
 	int revision;
